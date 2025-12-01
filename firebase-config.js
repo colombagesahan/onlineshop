9310a7b2fb53ebfb0d86b7ae8d779632ffda0384
colombagesahan <colombagesahan@gmail.com>
2025-12-01 15:48:11 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// --- PASTE YOUR FIREBASE KEYS HERE ---
 const firebaseConfig = {
   apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4",
   authDomain: "onlineshop-30cd1.firebaseapp.com",
@@ -14,33 +13,9 @@ const firebaseConfig = {
   measurementId: "G-6YLGGH2NDD"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-// --- THIS IS THE PART YOU WERE MISSING ---
-export async function getSiteSettings() {
-    try {
-        const docRef = doc(db, "settings", "general");
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            return docSnap.data();
-        } else {
-            return {
-                bizName: "My Shop",
-                primaryColor: "#333",
-                ownerPhone: "",
-                logoUrl: ""
-            };
-        }
-    } catch (error) {
-        console.error("Settings Error:", error);
-        return {};
-    }
-}
-
-// Export everything
 export { auth, db, storage };
