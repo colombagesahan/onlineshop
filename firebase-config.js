8c155e10c073073583f8c81480368df076e58ccd
colombagesahan <colombagesahan@gmail.com>
2025-12-03 16:23:25 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,16 +4,18 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-    // --- PASTE YOUR FIREBASE CONFIG KEYS HERE ---
-    apiKey: "AIzaSy...",
-    authDomain: "your-app.firebaseapp.com",
-    projectId: "your-app",
-    storageBucket: "your-app.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
+  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4",
+  authDomain: "onlineshop-30cd1.firebaseapp.com",
+  projectId: "onlineshop-30cd1",
+  storageBucket: "onlineshop-30cd1.firebasestorage.app",
+  messagingSenderId: "818252574868",
+  appId: "1:818252574868:web:8dd36825db589a886cc481",
+  measurementId: "G-6YLGGH2NDD"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
